d4764c6a344b35dd28eba0c2c0f272b4bc669aba
jjeffrin <jjeffrin@gmail.com>
2019-08-01 17:45:09 +0530

    Initial Commit
    This is sample

--- JquerySample/Controllers/HomeController.cs
+++ JquerySample/Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace JquerySample.Controllers
             return View();
         }
 
+        [HttpPost]
+        public void SubmitForm(UserModel userModel)
+        {
+            
+        }
+
         public ActionResult GetAgeOptions()
         {
             List<int> ageList = new List<int>();
@@ -20,8 +26,118 @@ namespace JquerySample.Controllers
             {
                 ageList.Add(age);
             }
-            return Json(ageList);
+            return Json(ageList, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult GetCountryOptions()
+        {
+            List<string> countryList = new List<string>();
+            countryList.Add("India");
+            countryList.Add("US");
+            countryList.Add("UK");
+            return Json(countryList, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetStateOptions(string country)
+        {
+            List<string> stateList = new List<string>() { "Tamil Nadu", "" };            
+            return Json(stateList, JsonRequestBehavior.AllowGet);
+        }
+
+
+            List<Country> countries = new List<Country>() { new Country() { CountryId = 1, CountryName = "India" },
+        new Country() {CountryId = 2, CountryName= "USA" },
+        new Country() {CountryId = 3, CountryName = "UK" } };
+
+            List<State> states = new List<State>() {
+                new State() { CountryId = 1, StateName = "Tamil Nadu", StateId = 1 },
+                new State() {CountryId = 1, StateName= "Kerala", StateId = 3 },
+                new State() {CountryId = 1, StateName = "AP", StateId = 2 },
+                new State() {CountryId = 2, StateName = "Georgia", StateId = 4 },
+                new State() {CountryId = 2, StateName = "California", StateId = 5 },
+                new State() {CountryId = 2, StateName = "Texas", StateId = 6 },
+                new State() {CountryId = 3, StateName = "City of London", StateId = 7 },
+                new State() {CountryId = 3, StateName = "Berkshire", StateId = 8 },
+                new State() {CountryId = 3, StateName = "Kent", StateId = 9 }
+            };
+
+            List<City> city = new List<City>() {
+                new City() { CityId = 1, CityName = "Chennai", StateId = 1 },
+                new City() {CityId = 2, CityName= "Coimbatore", StateId = 1 },
+                new City() {CityId = 3, CityName = "Vijayawada", StateId = 2 },
+                new City() {CityId = 4, CityName = "Vishakapatnam", StateId = 2 },
+                new City() {CityId = 5, CityName = "Cochin", StateId = 3 },
+                new City() {CityId = 6, CityName = "Trivandrum", StateId = 3 },
+                new City() {CityId = 7, CityName = "Atlanta", StateId = 4 },
+                new City() {CityId = 8, CityName = "Savannah", StateId = 4 },
+                new City() {CityId = 9, CityName = "Los Angeles", StateId = 5 },
+                new City() {CityId = 10, CityName = "San Francisco", StateId = 5 },
+                new City() {CityId = 11, CityName = "Dallas", StateId = 6 },
+                new City() {CityId = 12, CityName = "Austin", StateId = 6 },
+                new City() {CityId = 13, CityName = "Temple Church", StateId = 7 },
+                new City() {CityId = 14, CityName = "Fleet Street", StateId = 7 },
+                new City() {CityId = 15, CityName = "Windsor", StateId = 8 },
+                new City() {CityId = 16, CityName = "Hungerford", StateId = 8 },
+                new City() {CityId = 17, CityName = "Margate", StateId = 9 },
+                new City() {CityId = 18, CityName = "Canterbury", StateId = 9 },
+            };
+
+            public ActionResult GetCountries()
+            {
+                return Json(countries, JsonRequestBehavior.AllowGet);
+            }
+
+            public ActionResult GetState(int countryId)
+            {
+                return Json(states.Where(x => x.CountryId == countryId), JsonRequestBehavior.AllowGet);
+            }
+
+            public ActionResult GetCity(int stateId)
+            {
+                return Json(city.Where(x => x.StateId == stateId), JsonRequestBehavior.AllowGet);
+            }
+
+
+        public class City
+        {
+            public int CityId { get; set; }
+
+            public string CityName { get; set; }
+
+            public int StateId { get; set; }
+        }
+
+        public class State
+        {
+            public int StateId { get; set; }
+
+            public string StateName { get; set; }
+
+            public int CountryId { get; set; }
+        }
+
+        public class Country
+        {
+            public int CountryId { get; set; }
+            public string CountryName { get; set; }
+        }
+
+        public class UserModel
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string age { get; set; }
+            public string gender { get; set; }
+            public string address1 { get; set; }
+            public string address2 { get; set; }
+            public string country { get; set; }
+            public string state { get; set; }
+            public string city { get; set; }
+            public double mobileNumber { get; set; }
+            public string relocation { get; set; }
+            public string comment { get; set; }
+        }
+
 
         public ActionResult About()
         {
--- JquerySample/Scripts/main.js
+++ JquerySample/Scripts/main.js
@@ -7,8 +7,8 @@
     var lastName = $('#last-name');
     var age = $('#age');
     var gender = $('#gender');
-    var address1 = $('#address1');
-    var address2 = $('#address2');
+    var address1 = $('#address-1');
+    var address2 = $('#address-2');
     var country = $('#country');
     var state = $('#state');
     var city = $('#city')
@@ -16,22 +16,246 @@
     // $('input[name="relocation-radio"]:checked', '#user-reg-form').val()
     var comment = $('#comment');
 
-    // Populate Age details
-    $('#age').click(() => {
-        //sample
+    // Flag variables
+    var fNameCorrect, lNameCorrect, ageCorrect, genderCorrect, address1Correct, countryCorrect, stateCorrect, cityCorrect, mobileCorrect, commentCorrect;
+
+    // Listen for Name changes
+    firstName.on('input', () => {
+        if (firstName.val() == '') {
+            removeSalutation();
+        }
+        else {
+            setSalutation();
+            setGender();
+        }
     });
+    lastName.on('input', () => {
+        if (lastName.val() == '') {
+            removeSalutation();
+        }
+        else {
+            setSalutation();
+            setGender();
+        }
+    });
+    
+    // Theme Changing
+    $('#theme').click(() => {
+        if ($('#theme').text() == "Switch to Dark Mode") {
+            $('#background').css('background-color', 'black');
+            $('label').css('color', 'white');
+            $('h2').css('color', 'white');
+            $('#theme').html("Switch to Light Mode");
+        }
+        else {
+            $('#background').css('background-color', '#eeeeee');
+            $('label').css('color', 'black');
+            $('h2').css('color', 'black');
+            $('#theme').html("Switch to Dark Mode");
+        }
+    });
+
+
+
+    // Listen for Gender change
+    gender.change(() => {
+        setGender();
+    });
+
+    // Set Gender
+    function setGender() {
+        if (gender.val() == 'male') {
+            $('#gender-salutation').html("Mr. ");
+        }
+        if (gender.val() == 'female') {
+            $('#gender-salutation').html("Ms. ");
+        }
+    }
+
+    // Set Salutation
+    function setSalutation() {
+        $("#salutation").html("<hr>Welcome, <span id='gender-salutation'></span>" + firstName.val() + " " + lastName.val())
+    }
+
+    // Remove Salutation
+    function removeSalutation() {
+        $("#salutation").html('');
+    }
+
+    // Populate Age details
+    $.getJSON('Home/GetAgeOptions', (datas) => {
+        console.log(datas);
+        var content = '';
+        $.each(datas, (index, data) => {
+            content += "<option value=" + data + ">" + data + "</option>";
+        });
+        $('#age').html(content);
+    });
+
+    // Populate Country Details
+    $.getJSON('Home/GetCountries', (datas) => {
+        console.log(datas);
+        var content = '';
+        //$("#country").html("<option value='0'>Please choose a country</option>");
+        $.each(datas, (index, data) => {
+            content += "<option value=" + data.CountryId + ">" + data.CountryName + "</option>";
+        });
+        $('#country').html(content);
+        $('#country').prepend("<option value='0' selected>Please choose a country</option>");
+    });
+
+    // Populate State Details
+    $('#country').change(() => {
+        console.log($('#country').val());
+        $.getJSON('Home/GetState/?countryId='+$('#country').val(), (datas) => {
+            console.log(datas);
+            var content = '';
+            $('#city').html(content);
+            $.each(datas, (index, data) => {
+                content += "<option value=" + data.StateId + ">" + data.StateName + "</option>";
+            });
+            $('#state').html(content);
+            $('#state').prepend("<option value='0' selected>Please choose a state</option>");
+        });
+    });
+
+    // Populate City Details
+    $('#state').change(() => {
+        console.log($('#state').val());
+        $.getJSON('Home/GetCity/?stateId=' + $('#state').val(), (datas) => {
+            console.log(datas);
+            var content = '';
+            $.each(datas, (index, data) => {
+                content += "<option value=" + data.CityId + ">" + data.CityName + "</option>";
+            });
+            $('#city').html(content);
+        });
+    });
+
 
     // On Submit button click
     $('#btn-submit').click(() => {
-        console.log('Submit');
+        // Check First Name
+        if (!(firstName.val().length <= 50 && firstName.val() != "" && /^[A-Za-z]+$/.test(firstName.val()))) {
+            $('#first-name-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please enter valid values.</div>");
+            fNameCorrect = false;
+        } else {
+            $('#first-name-error').html('');
+            fNameCorrect = true;
+        }
+
+        // Check Last Name
+        if (!(lastName.val().length <= 50 && lastName.val() != "" && /^[A-Za-z]+$/.test(lastName.val()))) {
+            $('#last-name-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please enter valid values.</div>");
+            lNameCorrect = false;
+        } else {
+            $('#last-name-error').html('');
+            lNameCorrect = true;
+        }
+
+        // Check Address Line 1
+        if (!(address1.val().length <= 150 && lastName.val() != "")) {
+            $('#address-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please enter valid values.</div>");
+            address1Correct = false;
+        } else {
+            $('#address-error').html('');
+            address1Correct = true;
+        }
+
+        // Check Gender
+        console.log(gender.val());
+        if (gender.val() == 0) {
+            $('#gender-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please select a gender.</div>");
+            genderCorrect = false;
+        }
+        else {
+            $('#gender-error').html('');
+            genderCorrect = true;
+        }
+
+        // Check Country
+        console.log(country.val());
+        if (country.val() == 0) {
+            $('#country-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please select a country.</div>");
+            countryCorrect = false;
+        }
+        else {
+            $('#country-error').html('');
+            countryCorrect = true;
+        }
+
+        // Check State
+        console.log(state.val());
+        if (state.val() == null) {
+            $('#state-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please select a state.</div>");
+            stateCorrect = false;
+        }
+        else {
+            $('#state-error').html('');
+            stateCorrect = true;
+        }
+
+        // Check City
+        if (city.val() == null) {
+            $('#city-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please select a city.</div>");
+            cityCorrect = false;
+        }
+        else {
+            $('#city-error').html('');
+            cityCorrect = true;
+        }
+
+        // Check Mobile Number 
+        if (!/^[6-9]{1}[0-9]{9}$/.test(mobileNumber.val())) {
+            $('#mobile-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please enter a valid mobile number.</div>");
+            mobileCorrect = false;
+        }
+        else {
+            $('#mobile-error').html('');
+            mobileCorrect = true;
+        }
+
+        // Check Comment        
+        if (comment.val().length > 1000 && !comment.val() == null) {
+            $('#comment-error').html("<div class='alert alert-danger show col-sm-6' role='alert'>Please enter only 1000 characters.</div>");
+            commentCorrect = false;
+        }
+        else {
+            $('#comment-error').html('');
+            commentCorrect = true;
+        }
+
+        // Submit
+        console.log(fNameCorrect, lNameCorrect, genderCorrect, address1Correct, countryCorrect, stateCorrect, cityCorrect, mobileCorrect, commentCorrect);
+        if (fNameCorrect && lNameCorrect && genderCorrect && address1Correct && countryCorrect && stateCorrect && cityCorrect && mobileCorrect && commentCorrect) {
+            console.log('correct');
+            var userDetails = {
+                firstName: firstName.val(),
+                lastName: lastName.val(),
+                age: age.val(),
+                gender: gender.val(),
+                address1: address1.val(),
+                address2: address2.val(),
+                country: country.find("option[value=" + country.val() + "]").text(),
+                state: state.find("option[value="+state.val()+"]").text(),
+                city: city.find("option[value=" + city.val() + "]").text(),
+                mobileNumber: mobileNumber.val(),
+                relocation: $('input[name="relocation-radio"]:checked', '#user-reg-form').val(),
+                comment: comment.val()
+            };
+            console.log(userDetails);
+            $.ajax({
+                type: "POST",
+                url: "Home/SubmitForm",
+                data: userDetails,
+                traditional: true,
+                success: () => {
+                    console.log("Success");
+                }
+            });
+        }
+        else {
+            console.log('wrong');
+        }
     });
-
 });
-
-
-
-
-
-//function clearFields() {
-//    $('#user-reg-form').trigger('reset');
-//}
