68a8b7010af83dc2cb18def94883b3a9dd40e43a
jjeffrin <jjeffrin@gmail.com>
2019-08-01 10:07:47 +0530

    Add project files.

---
+++ JquerySample/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JquerySample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAgeOptions()
+        {
+            List<int> ageList = new List<int>();
+            for (int age = 18; age < 61; age++)
+            {
+                ageList.Add(age);
+            }
+            return Json(ageList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ JquerySample/Scripts/main.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+
+    console.log("Ready");
+
+    // Storing all form values in variables
+    var firstName = $('#first-name');
+    var lastName = $('#last-name');
+    var age = $('#age');
+    var gender = $('#gender');
+    var address1 = $('#address1');
+    var address2 = $('#address2');
+    var country = $('#country');
+    var state = $('#state');
+    var city = $('#city')
+    var mobileNumber = $('#mobile-number');
+    // $('input[name="relocation-radio"]:checked', '#user-reg-form').val()
+    var comment = $('#comment');
+
+    // Populate Age details
+    $('#age').click(() => {
+
+    });
+
+    // On Submit button click
+    $('#btn-submit').click(() => {
+        console.log('Submit');
+    });
+
+});
+
+
+
+
+
+//function clearFields() {
+//    $('#user-reg-form').trigger('reset');
+//}
